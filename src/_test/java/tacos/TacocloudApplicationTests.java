52504de145069a38ee61768d7b313dea5730ed05
unknown <minchang6335@gmail.com>
2025-06-03 06:45:41 +0900

    first commit

--- src/_test/java/tacos/TacocloudApplicationTests.java
+++ src/_test/java/tacos/TacocloudApplicationTests.java
@@ -2,8 +2,10 @@ package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TacocloudApplicationTests {
 
     @Test
